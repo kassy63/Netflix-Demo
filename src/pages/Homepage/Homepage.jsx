1b1b68813aa609673b0064698548c7f98e33d8a0
kassy <sky122413@gmail.com>
2024-09-03 02:38:47 +0900

    영화 슬라이드 리스트

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Banner from "./components/Banner/Banner";
+import PopularMovieSlide from "./components/Banner/PopularMovieSlide/PopularMovieSlide";
+import TopRatedMovieSlide from "./components/Banner/TopRatedMovieSlide/TopRatedMovieSlide";
+import UpcomingMovieSlide from "./components/Banner/UpcomingMovieSlide/UpcomingMovieSlide";
 
 // 1. 배너 -> popular 영화를 들고와서 첫번째 아이템을 보여주자
 // 2. popular movie
@@ -10,6 +13,9 @@ const Homepage = () => {
   return (
     <div>
       <Banner />
+      <PopularMovieSlide />
+      <TopRatedMovieSlide />
+      <UpcomingMovieSlide />
     </div>
   );
 };
