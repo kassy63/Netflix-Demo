e9020fbb9e6efef55089a7506ad0f3b7a65bbd9c
kassy <sky122413@gmail.com>
2024-09-04 01:50:34 +0900

    List별 Slide 수정

--- src/pages/Homepage/components/Banner/UpcomingMovieSlide/UpcomingMovieSlide.jsx
+++ src/pages/Homepage/components/Banner/UpcomingMovieSlide/UpcomingMovieSlide.jsx
@@ -1,28 +1,7 @@
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import "./UpcomingMovieSlide.style.css";
+import { responsive } from "../../../../../constants/responsive";
+import MovieSlider from "../../../../../common/MovieSlider/MovieSlider";
 import { useUpcomingMoviesQuery } from "../../../../../hooks/useUpcommingMovies";
 import { Alert } from "react-bootstrap";
-import MovieCard from "../../MovieCard/MovieCard";
-
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    // items: 3,
-    items: 6,
-    // slidesToSlide: 3, // optional, default to 1.
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-    // slidesToSlide: 2, // optional, default to 1.
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-    // slidesToSlide: 1, // optional, default to 1.
-  },
-};
 
 const UpcomingMovieSlide = () => {
   const { data, isLoading, isError, error } = useUpcomingMoviesQuery();
@@ -35,35 +14,11 @@ const UpcomingMovieSlide = () => {
   }
   return (
     <div>
-      <h3>예정 영화</h3>
-      <Carousel
-        // swipeable={false}
-        // draggable={false}
-        // showDots={true}
-        // responsive={responsive}
-        // ssr={true} // means to render carousel on server-side.
-        // infinite={true}
-        // autoPlay={this.props.deviceType !== "mobile" ? true : false}
-        // autoPlaySpeed={1000}
-        // keyBoardControl={true}
-        // customTransition="all .5"
-        // transitionDuration={500}
-        // containerClass="carousel-container"
-        // removeArrowOnDeviceType={["tablet", "mobile"]}
-        // deviceType={this.props.deviceType}
-        // dotListClass="custom-dot-list-style"
-        // itemClass="carousel-item-padding-40-px"
-        infinite={true}
-        centerMode={true}
-        itemClass="movie-slider p-1"
-        containerClass="carousel-container"
+      <MovieSlider
+        title="예정 영화"
+        movies={data.results}
         responsive={responsive}
-      >
-        {data.results.map((movie, index) => (
-          <MovieCard movie={movie} key={index} />
-        ))}
-      </Carousel>
-      ;
+      />
     </div>
   );
 };
